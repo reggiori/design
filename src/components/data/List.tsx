61d3f77252015dbb2318cc8b773af4ca847f9411
Mario Reggiori <git@marioreggiori.com>
2021-07-10 15:44:37 +0200

    dev

--- src/components/data/List.tsx
+++ src/components/data/List.tsx
@@ -1,30 +1,43 @@
-import {ComponentProps, JSXElementConstructor} from 'react'
-import {}from 'styled-components/macro'
+import { ComponentProps, JSXElementConstructor } from 'react'
+import { } from 'styled-components/macro'
 
 interface Props<T extends JSXElementConstructor<any>> {
   component: T
   data: ComponentProps<T>[]
 }
 
-export function List<T extends JSXElementConstructor<any>>({data,component:Component}:Props<T>){
+export function List<T extends JSXElementConstructor<any>>({ data, component: Component }: Props<T>) {
   return (
     <ul css={`
     list-style-type:none;
     padding:0;
     margin:0;
+    overflow-y:auto;
+    flex:1;
     `}>
-      {data.map((itemData, index)=><Component key={index} {...itemData} />)}
+      {data.map((itemData, index) => <Component key={index} {...itemData} />)}
     </ul>
   )
 }
 
-export const ListItem = ({title,content}:{title:string, content:any})=>{
+export const ListItem = ({ title, content }: { title: string, content: any }) => {
   return (
     <li css={`
-    border-bottom:1px solid #000;
+    border-bottom:1px solid var(--border-color);
+    padding:8px;
+
+    &>div{
+      white-space:nowrap;
+      overflow:hidden;
+      text-overflow:ellipsis;
+    }
+
+    &:last-child{
+      border-bottom:none;
+    }
     `}>
-      <b>{title}</b>
-      <p>{content}</p>
+      <div css="font-weight:bold;">{title}</div>
+      <div>{content}</div>
     </li>
   )
 }
--- src/stories/data/List.tsx
+++ src/stories/data/List.tsx
@@ -1,18 +1,27 @@
-import { ComponentStory,ComponentMeta } from '@storybook/react'
-import {List, ListItem} from 'components'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { List, ListItem } from 'components'
+import { } from 'styled-components/macro'
+import faker from 'faker'
 
 export default {
-  title:'Data/List',
+  title: 'Data/List',
   component: List,
 } as ComponentMeta<typeof List>
 
-const Template: ComponentStory<typeof List> = (args) =><List {...args}/>
+const Template: ComponentStory<typeof List> = (args) => (
+  <div css={`
+    max-width:200px;
+    border:1px solid var(--border-color);
+    max-height:calc(100vh - 2rem);
+    height:520px;
+    display:flex;
+    `}>
+    <List {...args} />
+  </div>
+)
 
 export const Primary = Template.bind({})
 Primary.args = {
-  data:[
-    {title:'Headline 1', content:'Content 1'},
-    {title:'Headline 2', content:'Content 2'},
-  ],
+  data: new Array(50).fill(null).map(() => ({ title: faker.name.findName(), content: faker.lorem.sentence(5, 10) })),
   component: ListItem,
 }
